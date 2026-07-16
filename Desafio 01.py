8126b02438c2349315a2aa581ee2599b607804dc
7Nico <nicfariao@gmail.com>
2026-07-16 10:39:01 -0300

    Meus primeiros desafios de Python

---
+++ Desafio 01.py
@@ -0,0 +1,3 @@
+print('====== DESAFIO 01 ======')
+nome = input('Olá! Qual é o seu nome?')
+print('Olá', nome, '!' 'Prazer em te conhecer!')
---
+++ Desafio 03.py
@@ -0,0 +1,5 @@
+print('====== DESAFIO 03 ======')
+primeiro = int(input('Primeiro número'))
+segundo = int(input('Segundo número'))
+soma = sum([primeiro, segundo])
+print('A soma é', soma)
