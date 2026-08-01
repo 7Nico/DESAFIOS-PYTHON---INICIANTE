041e44ba29331bdd622548833bfc2a49be757e31
Nicolas Faria Oliveira <nicfariao@gmai.com>
2026-08-01 13:35:54 -0300

    Novos desafios

---
+++ Desafio 48.py
@@ -0,0 +1,6 @@
+# Faça um programa que calcule a soma entre todos os números que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+for c in range (3, 501, 3):
+    (c)
+
+print(c)
