6dcb135a6321754af28cfecfc0a439927c0569b9
7Nico <nicfariao@gmail.com>
2026-08-03 18:35:49 -0300

    extras

--- Desafio 55.py
+++ Desafio 55.py
@@ -1,1 +1,6 @@
-# Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.    
+# Faça um programa que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+
+for p in range(1, 6):
+    peso = float(input('Digite o seu peso: '))
+    if peso > peso:
